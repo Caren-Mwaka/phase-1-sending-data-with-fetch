044ffff8841de82d4ad711593f08d25764c4ecf8
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-04-12 12:34:31 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Unauthorized Access");
+      }
+      return response.json();
+    })
+    .then(data => {
+      appendNewIdToDOM(data.id);
+    })
+    .catch(error => {
+      appendErrorMessageToDOM(error.message);
+    });
+  }
+  
+  function appendNewIdToDOM(id) {
+    const newElement = document.createElement("div");
+    newElement.innerHTML = id;
+    document.body.appendChild(newElement);
+  }
+  
+  function appendErrorMessageToDOM(message) {
+    const errorElement = document.createElement("div");
+    errorElement.innerHTML = message;
+    document.body.appendChild(errorElement);
+  }
+  
